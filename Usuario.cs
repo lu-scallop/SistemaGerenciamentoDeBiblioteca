52034bd9e711ce12ffa9573bf71fee3efb91b795
Lucas Vieira Rocha <lucasviro023@gmail.com>
2025-07-08 13:33:31 -0300

    Add: Classe Usuario

---
+++ Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGerenciamentoDeBiblioteca
+{
+	public class Usuario
+	{
+		public string Id { get; set; }
+		public string Nome { get; set; }
+
+		public int Idade { get; set; }
+
+		public bool PodePegarLivroEmprestado { get; set; }
+	}
+}
